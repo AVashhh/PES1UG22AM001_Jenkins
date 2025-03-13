385aec51a6f77cc7a132e764a511c4c064638cfd
AVashhh <143221190+AVashhh@users.noreply.github.com>
2025-03-13 17:09:55 +0530

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run this  \n";
+  cout << "I have modified this  \n";
+  cout << "I AM CRYING cause of how many times I had to redo this  \n";
+  return 0;
+}
